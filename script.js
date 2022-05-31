5c94f5c0b8bba578070268eddc53d38cd2037690
Krzysztof-K94 <krzysztofkukielka94@gmail.com>
2022-05-31 16:14:06 +0200

    async function

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 
+const container = document.querySelector('.advice');
 const adviceContainer = document.querySelector('.advice__box');
 const btn = document.querySelector('.advice__dice');
+const heading = document.querySelector('.advice__heading');
+const text = document.querySelector('.advice__paragraph');
+const loader = document.querySelector('.loader');
 
+const loading = function() {
+    loader.hidden = false;
+    container.classList.add('hidden');
+}
 
-const render = function(data) {
+const complete = function() {
+    container.classList.remove('hidden');
+    loader.hidden = true;
+}
+
+const getQuote = async function() {
+
+    try {
+        const response = await fetch('https://api.adviceslip.com/advice');
+        const quote = await response.json();
+        return quote;
+    }
+    catch(err) {
+        console.log(err.message)
+    }  
+    
+};
+
+const render = async function() {
+ 
+    loading()
+    try {
+    const data = await getQuote();
     const html = 
     `
     <h1 class="advice__heading">Advice ${data.slip.id}</h1>
@@ -12,10 +42,15 @@ const render = function(data) {
 
     adviceContainer.innerHTML = " ";
     adviceContainer.insertAdjacentHTML('afterbegin', html);
+    }
+    catch(err) {
+        console.log(err.message)
+    }
+    complete();
 }
 
-btn.addEventListener('click', function(){
-    fetch('https://api.adviceslip.com/advice')
-    .then( res => res.json())
-    .then(data => render(data));
-});
+
+
+render();
+
+btn.addEventListener('click', render);
